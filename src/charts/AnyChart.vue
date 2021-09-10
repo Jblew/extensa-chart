4e634976fc28687aa19e0bfba1f2cc3abefea76e
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2021-09-10 17:59:30 +0200

    feat: any chart switch

--- src/charts/AnyChart.vue
+++ src/charts/AnyChart.vue
@@ -1,23 +1,41 @@
 <template>
-  <div>
-    <code>{{ configJSON }}</code>
-  </div>
+  <component :is="component" :chart="chart" />
 </template>
 
 <script lang="ts">
-import { Chart } from "@/types";
+import { Chart, AnyChart as TAnyChart } from "@/types";
 import { Component, Vue, Prop } from "vue-property-decorator";
-import AnyChart from "@/charts/AnyChart.vue";
+import PieChart from "@/charts/PieChart.vue";
 
 @Component({
-  components: { AnyChart },
+  components: { PieChart },
 })
 export default class View extends Vue {
   @Prop()
-  config?: Chart;
+  config!: Chart;
+
+  components: Record<string, any> = {
+    pie: "PieChart",
+  };
 
   get configJSON(): string {
     return JSON.stringify(this.config || {}, undefined, 2);
+  }
+
+  get chart(): TAnyChart {
+    return this.config.chart;
+  }
+
+  get type(): string {
+    return this.chart.type;
+  }
+
+  get component(): any {
+    const component = this.components[this.type];
+    if (!component) {
+      throw new Error(`Unknown chart type: ${this.type}`);
+    }
+    return component;
   }
 }
 </script>
---
+++ src/charts/PieChart.vue
@@ -0,0 +1,19 @@
+<template>
+  <strong>PieChart</strong>
+</template>
+
+<script lang="ts">
+import { PieChart } from "@/types";
+import { Component, Vue, Prop } from "vue-property-decorator";
+
+@Component
+export default class View extends Vue {
+  @Prop()
+  chart!: PieChart;
+}
+</script>
+<style scoped>
+.error {
+  color: red;
+}
+</style>
